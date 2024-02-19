d57a8ab8445129d5e6e14e7952f6afdf8471a2fc
Azubi forex <noel.osiro@azubiafrica.org>
2024-02-19 13:07:34 +0300

    Header data

--- my-app/src/components/Header/Header.tsx
+++ my-app/src/components/Header/Header.tsx
@@ -1,29 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { links } from '../../data/headerData';
 
-const Header = () => {
-    return (
-        <header id="header" className="fixed-top d-flex align-items-center">
-            <div className="container d-flex justify-content-between align-items-center">
+const Header: React.FC = () => {
 
-                <div className="logo">
-                    <h1><a href="index.html">MaliCul</a></h1>
-                    <a href="index.html">
-                        <img src="assets/img/logo.png" alt="" className="img-fluid" />
-                    </a>
-                </div>
 
-                <nav id="navbar" className="navbar">
-                    <ul>
-                        <li><a className="active " href="/home">Home</a></li>
-                        <li><a href="features.html">Features</a></li>
-                        <li><a href="blog.html">Blog</a></li>
-                        <li><a href="contact.html">Contact Us</a></li>
-                    </ul>
-                    <i className="bi bi-list mobile-nav-toggle"></i>
-                </nav>
-            </div>
-        </header>
-    )
+  return (
+    <header id="header" className="fixed-top d-flex align-items-center">
+      <div className="container d-flex justify-content-between align-items-center">
+        <div className="logo">
+          <h1><a href="/home">MaliCul</a></h1>
+          {/* <a href="/home">
+            <img src="assets/img/logo.png" alt="" className="img-fluid" />
+          </a> */}
+        </div>
+
+        <nav id="navbar" className="navbar">
+          <ul>
+            {links.map((link, index) => (
+              <li key={index}>
+                <a className={window.location.pathname === link.url ? "active" : ""} href={link.url}>{link.title}</a>
+              </li>
+            ))}
+          </ul>
+          <i className="bi bi-list mobile-nav-toggle"></i>
+        </nav>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
---
+++ my-app/src/data/headerData.ts
@@ -0,0 +1,11 @@
+interface Link {
+    title: string;
+    url: string;
+}  
+
+export   const links: Link[] = [
+    { title: "Home", url: "/home" },
+    { title: "Features", url: "features.html" },
+    { title: "Blog", url: "blog.html" },
+    { title: "Contact Us", url: "contact.html" }
+  ];
